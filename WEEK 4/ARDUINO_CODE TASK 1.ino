2a6dc4d5ba927bcb56d4a9edd9cc1a8ac725b2a0
firdaushsni <afhusaini04@gmail.com>
2025-11-05 07:24:28 +0800

    Create ARDUINO_CODE TASK 1.ino

---
+++ WEEK 4/ARDUINO_CODE TASK 1.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 connection failed!");
+    while (1);
+  }
+  Serial.println("MPU6050 ready");
+}
+
+void loop() {
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  // Send comma-separated accelerometer data
+  Serial.print(ax); Serial.print(",");
+  Serial.print(ay); Serial.print(",");
+  Serial.println(az);
+
+  delay(50);   // 20 samples per second
+}
+
+
+
